8e9b0d4faead96ccdeb617618d3a064efe28f329
James Almond <jamestalmond@gmail.com>
2019-02-04 15:44:42 +0000

    React counter example

---
+++ src/createStoreFromScratch.js
@@ -0,0 +1,23 @@
+// very close to the createStore method shipped with redux
+const createStore = reducer => {
+	let state;
+	let listeners = [];
+
+	const getState = () => state;
+
+	const dispatch = action => {
+		state = reducer(state, action);
+		listeners.forEach(listener => listener());
+	};
+
+	const subscribe = listener => {
+		listeners.push(listener);
+		return () => {
+			listeners = listeners.filter(l => l !== listener);
+		};
+	};
+
+	dispatch({});
+
+	return { getState, dispatch, subscribe };
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './index.css';
 
-// import { createStore } from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 
 const counter = (state = 0, action) => {
 	switch (action.type) {
@@ -13,43 +15,34 @@ const counter = (state = 0, action) => {
 	}
 };
 
-// very close to the createStore method shipped with redux
-const createStore = reducer => {
-	let state;
-	let listeners = [];
-
-	const getState = () => state;
-
-	const dispatch = action => {
-		state = reducer(state, action);
-		listeners.forEach(listener => listener());
-	};
-
-	const subscribe = listener => {
-		listeners.push(listener);
-		return () => {
-			listeners = listeners.filter(l => l !== listener);
-		};
-	};
-
-	dispatch({});
-
-	return { getState, dispatch, subscribe };
-};
+const Counter = ({ value, onIncrement, onDecrement }) => (
+	<div>
+		<h1>{value}</h1>
+		<button onClick={onIncrement}>+</button>
+		<button onClick={onDecrement}>-</button>
+	</div>
+);
 
 const store = createStore(counter);
 
 const render = () => {
-	document.body.innerText = store.getState();
+	ReactDOM.render(
+		<Counter
+			value={store.getState()}
+			onIncrement={() =>
+				store.dispatch({
+					type: 'INCREMENT'
+				})
+			}
+			onDecrement={() =>
+				store.dispatch({
+					type: 'DECREMENT'
+				})
+			}
+		/>,
+		document.getElementById('app')
+	);
 };
 
 store.subscribe(render);
 render();
-
-store.subscribe(() => {
-	document.body.innerText = store.getState();
-});
-
-document.addEventListener('click', () => {
-	store.dispatch({ type: 'INCREMENT' });
-});
